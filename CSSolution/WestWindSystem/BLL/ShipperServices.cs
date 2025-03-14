57c715bfb46e0b38d202f500f0a64ed8f0cd6c20
Don Welch <dwelch@nait.ca>
2025-03-14 14:45:59 -0600

    complete Region events on web page, Create shipper and shipment services

---
+++ CSSolution/WestWindSystem/BLL/ShipperServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region additional namespaces
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+#endregion
+
+namespace WestWindSystem.BLL
+{
+    public class ShipperServices
+    {
+        #region setup of the context connection variable and class constructor
+
+        private readonly WestWindContext _context;
+
+        internal ShipperServices(WestWindContext registeredcontext)
+        {
+            _context = registeredcontext;
+        }
+        #endregion
+
+        #region Services
+        public List<Shipper> Shipper_GetAll()
+        {
+            IEnumerable<Shipper> info = _context.Shippers;
+            return info.ToList();
+        }
+        #endregion
+    }
+}
--- CSSolution/WestWindSystem/WestWindExtensions.cs
+++ CSSolution/WestWindSystem/WestWindExtensions.cs
@@ -59,6 +59,16 @@ namespace WestWindSystem
                 var context = serviceProvider.GetService<WestWindContext>();
                 return new RegionServices(context);
             });
+            services.AddTransient<ShipperServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+                return new ShipperServices(context);
+            });
+            services.AddTransient<ShipmentServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+                return new ShipmentServices(context);
+            });
         }        
     }
 }
